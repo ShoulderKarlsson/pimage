11b71ee9d4aadaeae5343ca78171cc20bc29c5ef
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-07-07 00:42:47 +0200

    feat: presentation of folders returned form server

--- ui/src/components/sidebar-link.js
+++ ui/src/components/sidebar-link.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import {MdFolder, MdFolderOpen} from 'react-icons/lib/md'
-import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import styled from 'styled-components'
 
-export const SidebarLink = ({...props}) => {
+
+const SidebarLinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: orange;
+  margin: 10px;
+  height: 100px;
+  width: 100px;
+`
+
+// const StyledLink = styled(Link)`
+//   text-decoration: none;
+//   color: ${props => props.color ? props.color : 'initial'};
+//   marginLeft: 3px;
+//   background-color: purple;
+// `
+//
+
+
+export const SidebarLink = withRouter(({name, path, isActive, history, ...props}) => {
   console.log(props)
   return (
-    <div>
-      <Link to={props.name}>
-        <p>Hej</p>
-      </Link>
-    </div>
+    <SidebarLinkContainer onClick={() => history.push(path)}>
+      <div className='folder-container'>
+        {isActive ? <MdFolderOpen /> : <MdFolder />}
+      </div>
+      <div className='name-container'>
+        <p>{name}</p>
+      </div>
+    </SidebarLinkContainer>
   )
-}
+})
--- ui/src/components/sidebar.js
+++ ui/src/components/sidebar.js
@@ -4,23 +4,40 @@ import {compose} from 'recompose'
 import {withFetch} from 'with-fetch'
 import fetch from 'isomorphic-fetch'
 import {SidebarLink} from './sidebar-link.js'
+import {Loading} from './loading.js'
 
 const SidebarContainer = styled.div`
   flex: ${props => props.flex ? props.flex : 1};
   height: 100vh;
   flex-direction: column;
+  background-color: yellow;
 `
 
 const enhance = compose(
-  withFetch(() => fetch('http://localhost:5000/images')),
+  withFetch(() => fetch('http://localhost:5000/images'))
 )
 
 export const Sidebar = enhance(({data, loading, ...props}) => {
+  console.log('rendering sidebar')
   return (
     <SidebarContainer>
-    {loading ? <p>Waiting..</p> : (
-      data.body.map(({path, name}, i) => <SidebarLink key={i} name={name} path={path} {...props} />)
-    )}
+      {
+        loading ? (
+          <Loading />
+        ) : (
+          <React.Fragment>
+            {data.body.map(({path, name}, i) => (
+                <SidebarLink 
+                  isActive={props.location.pathname === path}
+                  path={path}
+                  name={name}
+                  key={i}
+                />
+              )
+            )}
+          </React.Fragment>
+        )
+      }
     </SidebarContainer>
   )
 })
