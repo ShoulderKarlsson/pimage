81063f771c26f054e3e68429694876b9fc6af7b3
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-07-13 06:52:01 +0200

    fix: added simple overlay, TODO: small styling fixes..

---
+++ ui/src/components/overlay.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const OverlayContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(56, 53, 53, 0.65);
+  display: flex;
+  position: fixed;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const Topbar = styled.div`
+  height: 10%;
+  width: 100%;
+  background-color: rgba(31, 34, 38, 0.65);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Content = styled.div`
+  height: 80%;
+  width: 80%;
+  margin-top: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Overlay = ({
+  children,
+  style = {},
+  onCloseButtonPress,
+  ...props
+}) => {
+  return (
+    <OverlayContainer style={style}>
+      <Topbar>
+        <div
+          onClick={onCloseButtonPress}
+          style={{ height: '20px', width: '20px', background: 'green' }}
+        />
+      </Topbar>
+      <Content>{children}</Content>
+    </OverlayContainer>
+  )
+}
