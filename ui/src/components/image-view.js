eb1b495603c65986fa1d2a72f483dfe6a2935c28
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-07-13 06:52:38 +0200

    feat: added overlay

--- ui/src/components/image-view.js
+++ ui/src/components/image-view.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import fetch from 'isomorphic-fetch'
 import styled from 'styled-components'
-import { compose, lifecycle, withState, withHandlers } from 'recompose'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+import lifecycle from 'recompose/lifecycle'
+import withState from 'recompose/withState'
+import { Overlay } from './overlay'
 
 const Image = styled.img`
-  height: 43%;
+  height: ${props => (props.height ? props.height : '43%')};
   width: auto;
   margin: 3px;
 `
 
 const enhance = compose(
   withState('images', 'setImages', []),
+  withState('activeImage', 'setActiveImage', ''),
   withHandlers({
     fetchImages: ({ location, setImages }) => () => {
       fetch(`http://localhost:5000${location.pathname}`)
@@ -28,25 +33,44 @@ const enhance = compose(
 
     componentDidUpdate(prevProps) {
       if (prevProps.location.pathname !== this.props.location.pathname) {
+
+        // Must close the full-preview of the image if the route changes
+        if (!!this.props.activeImage) {
+          this.props.setActiveImage('')
+        }
         this.props.fetchImages()
       }
     },
   }),
 )
 
-export const ImageView = enhance(({ images }) => {
+export const ImageView = enhance(({ images, activeImage, setActiveImage }) => {
   return (
-    <div style={{
-      height: '100vh',
-      width: '100%',
-      overflow: 'scroll',
-      display: 'flex',
-      justifyContent: 'space-around',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    }}>
+    <div
+      style={{
+        height: '100vh',
+        width: '100%',
+        overflow: 'scroll',
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      }}
+    >
+      {!!activeImage && (
+        <Overlay onCloseButtonPress={() => setActiveImage('')}>
+          <Image src={activeImage} height={'86%'} />
+        </Overlay>
+      )}
       {images.map((imagePath, i) => {
-        return <Image key={i} src={`http://localhost:5000${imagePath}`} />
+        const fullPath = `http://localhost:5000${imagePath}`
+        return (
+          <Image
+            onClick={() => setActiveImage(fullPath)}
+            key={i}
+            src={fullPath}
+          />
+        )
       })}
     </div>
   )
