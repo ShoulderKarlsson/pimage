7107d63468da6782876481cdf64862f2889cdb81
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-07-27 17:46:19 +0200

    fix: added position support for button

--- ui/src/components/overlay.js
+++ ui/src/components/overlay.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const translateButtonPosition = position => {
+  console.log('position', position)
+  // Default position
+  if (position === 'left') {
+    return 'flex-start'
+  } else if (position === 'right') {
+    return 'flex-end'
+  } else if (position === 'center') {
+    return position
+  } else {
+    // Not sure what to return as default?
+    return 'initial'
+  }
+}
+
 const OverlayContainer = styled.div`
   height: 100vh;
   width: 100%;
@@ -16,8 +31,9 @@ const Topbar = styled.div`
   width: 100%;
   background-color: rgba(31, 34, 38, 0.65);
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: ${props => translateButtonPosition(props.topbarButtonPosition)};
 `
 
 const Content = styled.div`
@@ -29,12 +45,23 @@ const Content = styled.div`
   align-items: center;
 `
 
-export const Overlay = ({ children, style = {}, onCloseButtonPress }) => (
+export const Overlay = ({
+  children,
+  style = {},
+  onCloseButtonPress,
+  topbarButtonPosition,
+}) => (
   <OverlayContainer className="overlay-container" style={style}>
-    <Topbar>
+    <Topbar topbarButtonPosition={topbarButtonPosition}>
       <div
         onClick={onCloseButtonPress}
-        style={{ height: '20px', width: '20px', background: 'green' }}
+        style={{
+          height: '20px',
+          width: '20px',
+          background: 'green',
+          marginLeft: '12px',
+          marginRight: '12px',
+        }}
       />
     </Topbar>
     <Content>{children}</Content>
