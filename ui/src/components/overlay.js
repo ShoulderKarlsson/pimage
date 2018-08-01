7efb22edf584c9f9c0f6dda9f719f160e42721e2
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-08-01 19:06:52 +0200

    feat: moved Bar into comon, removed not used function for position

--- ui/src/components/overlay.js
+++ ui/src/components/overlay.js
@@ -1,21 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-
-const translateButtonPosition = position => {
-  console.log('position', position)
-
-  // Default position
-  if (position === 'left') {
-    return 'flex-start'
-  } else if (position === 'right') {
-    return 'flex-end'
-  } else if (position === 'center') {
-    return position
-  } else {
-    // Not sure what to return as default?
-    return 'initial'
-  }
-}
+import { CrossWithLeftMargin, Bar } from './common'
 
 const OverlayContainer = styled.div`
   height: 100vh;
@@ -43,66 +28,8 @@ export const Overlay = ({
   buttonPosition,
 }) => (
   <OverlayContainer className="overlay-container" style={style}>
-    <Bar buttonPosition={buttonPosition} onButtonPress={onButtonPress} />
+    <Bar onButtonPress={onButtonPress} Icon={CrossWithLeftMargin} />
     <Content>{children}</Content>
   </OverlayContainer>
 )
 
-export const Bar = ({ buttonPosition, onButtonPress }) => {
-  return (
-    <div
-      style={{
-        height: '10%',
-        width: '100%',
-        backgroundColor: 'rgba(31, 34, 38, 0.65)',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: translateButtonPosition(buttonPosition),
-      }}
-    >
-      <div
-        onClick={onButtonPress}
-        style={{
-          height: '20px',
-          width: '20px',
-          background: 'green',
-          marginLeft: '12px',
-          marginRight: '12px',
-        }}
-      />
-    </div>
-  )
-}
-
-const crossStyling = `
-.cross {
-  background: yellow;
-  top: 100px;
-  left: 100px;
-  position: absolute;
-  height: 50px;
-  width: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.cross:before, .cross:after {
-  content: "";
-  position: absolute;
-  background: orange;
-}
-
-.cross:before {
-  height: 10px;
-  width: 50px;
-  transform: rotate(45deg);
-}
-
-.cross:after {
-  height: 10px;
-  width: 50px;
-  transform: rotate(-45deg);
-}
-`
